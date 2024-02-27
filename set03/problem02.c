7a6a5ee21e2b100bc133a49c4bc3e863c7ceaba1
khushi <khushik.bsc23@rvu.edu.in>
2024-02-27 13:00:42 +0530

    test

--- set03/problem02.c
+++ set03/problem02.c
@@ -1,37 +1,54 @@
+/*
+2. Write a program to find whether the given 3 points form a triangle
+
+***Function Declarations***
+```c
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3);
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3);
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result);*/
+
 #include<stdio.h>
-int input(){
-    int a;
-    printf("Enter the sides\n");
-    scanf("%d",&a);
-    return a;
-    
-}
- int check_scalene(int a,int b,int c){
-if(a!=b&& a!=c && b!=c){
-    printf("Its a scalene triangle\n");
-    return 1;
+void input_triangle(float *x1, float *y1, float *x2, float *y2, float *x3, float *y3){
+    printf("Enter the x-coordinate of triangle A\n:");
+    scanf("%f",x1);
+    printf("Enter the y-coordinate of triangle A\n:");
+    scanf("%f",y1);
+     printf("Enter the x-coordinate of triangle B\n:");
+    scanf("%f",x2);
+    printf("Enter the y-coordinate of triangle B\n:");
+    scanf("%f",y2);
+     printf("Enter the x-coordinate of triangle C\n:");
+    scanf("%f",x3);
+    printf("Enter the y-coordinate of triangle C\n:");
+    scanf("%f",y3);
+
 
 }
-else{
-    printf("Its a not a scalene traingle\n");
-}
-return 0;
- }
- void output(int a, int b, int c, int isscalene){
-    if(isscalene){
-        printf("The triangle is scalene\n");
+int is_triangle(float x1, float y1, float x2, float y2,float x3, float y3){
+    float side1=sqrt(pow(x2-x1, 2)+ pow(y2-y1, 2));
+    float side2=sqrt(pow(x3-x1, 2)+ pow(y3-y1, 2));
+    float side3=sqrt(pow(x3-x2, 2)+ pow(y3-y2, 2));
+    if(side1+side2>side3 && side2+side3>side1 && side3+side1>side2){
+        return 1;
     }
     else{
-        printf("The triangle is not scalene");
+        return 0;
     }
- }
- int main(){
-    int a,b,c;
-    a = input();
-    b = input();
-    c = input();
-    check_scalene(a,b,c);
-    int isscalene;
-    output(a,b,c,isscalene);
+}
+void output(float x1, float y1, float x2, float y2,float x3, float y3, int result){
+    if(result==1){
+        printf("The points A,B,C forms a triangle");
+
+
+    }
+    else if(result==0){
+        printf("The points A,B,C do not forms a triangle")
+    }
+}
+int main(){
+    float x1, y1, x2, y2, x3, y3,result;
+    input_triangle(&x1, &y1, &x2, &y2, &x3, &y3);
+    triangle=is_triangle(x1,y1,x2,y2,x3,y3);
+    output(x1,y1,x2,y2,x3,y3,result);
     return 0;
- }
+}
