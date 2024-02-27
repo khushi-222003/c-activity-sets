7410f6839a5ade835796d7514678f59b7d186b70
khushi <khushik.bsc23@rvu.edu.in>
2024-02-27 14:05:10 +0530

    test

--- set03/problem03.c
+++ set03/problem03.c
@@ -9,34 +9,35 @@ void output(int n, int result);*/
 
 #include<stdio.h>
 int input_number(){
-    int num;
+    int n;
     printf("Enter the number:");
-    scanf("%d",&num);
-    return num;
+    scanf("%d",&n);
+    return n;
 }
+
 int is_prime(int n){
     for(int i=2; i<=n/2; i++){
         if(n%i==0){
             return 0;
         }
         else{
-    
-    return 1;
+            return 1;
         }
+    }
 }
-}
-void output(int n ,int result){
+void output(int n, int result){
     if(result==1){
-        printf("%d is a prime number", n);
+        printf("The numnber is a prime number\n");
     }
     else{
-        printf("%d is not a prime number",n);
+        printf("The number is not a prime number\n");
     }
+
 }
 int main(){
-    int num, result;
-    num=input_number();
-    result=is_prime(num);
-    output(num,result);
+    int n,result;
+    n=input_number();
+    result=is_prime(n);
+    output(n,result);
     return 0;
 }
